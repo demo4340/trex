7b12a5e5a681cfff26abdbd74c1a6ea986cd9bbe
demo4340 <70212084+demo4340@users.noreply.github.com>
2020-08-25 19:22:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  
+  createCanvas(400, 400);
+  
+}
+
+function draw() {
+  background("blue");
+  rect(370,mouseY,20,100);
+  rect(10,150,20,100);
+  rect(200,200,20,20);
+  
+  
+  
+}
